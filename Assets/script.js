874b48a962feefc2d41947011b38fce08dd43e86
Vin Rodriguez <vince.ray90@gmail.com>
2021-03-01 22:52:01 -0600

    styled the save buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,9 @@
 // use moment js to display current date
 var today = moment().format("MMMM Do, YYYY");
 $("#currentDay").text(today);
+
+$("button").click(function(){
+console.log(this);
+console.log($('.input-group span').first())
+
+})
